e516cb6ecde62d2008fee57ac8f5f9314b66f359
Gabor Szita <gaborszita88@gmail.com>
2023-08-23 07:32:21 -0700

    Add RPLIDAR SDK and Adafruit motor hat library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include <rplidar.h>
+#include "Adafruit_MotorHAT.h"
+
+Adafruit_MotorHAT hat;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  rp::standalone::rplidar::RPlidarDriver* lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver();
+  return 0;
 }
