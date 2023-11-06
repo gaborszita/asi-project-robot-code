911ad6255346bd4c7ad3f23f185e934a54916ba1
Gabor Szita <gaborszita88@gmail.com>
2023-11-05 17:32:28 -0800

    LidarLogManager.cpp handle lidar error

--- src/logging/LidarLogManager.cpp
+++ src/logging/LidarLogManager.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <boost/log/utility/manipulators/add_value.hpp>
 #include "Utilities/TimeManager.hpp"
+#include <iostream>
 
 namespace logging = boost::log;
 namespace attrs = boost::log::attributes;
@@ -43,36 +44,51 @@ void LidarLogManager::quickStop() {
 
 void LidarLogManager::update() {
   std::unique_lock<std::mutex> lock(threadMutex);
-  while (true) {
+  int failedToGetScanDataCount = 0;
+  while (!threadInterrupt) {
     m_nodeCount = sizeof(m_nodes)/sizeof(rplidar_response_measurement_node_hq_t);
     lock.unlock();
     std::unique_lock<std::shared_mutex> lock2(lidarDataReadMutex);
-    lidarManager.grabScanData(m_nodes, m_nodeCount);
-    lock2.unlock();
-    lock.lock();
-    if (threadInterrupt) {
-      break;
-    }
     auto timeNow = std::chrono::system_clock::now();
-
-    std::stringstream lidarDataStream;
-    for (int i = 0; i < m_nodeCount; i++) {
-      float angle_in_degrees = m_nodes[i].angle_z_q14 * 90.f / (1 << 14);
-      float distance_in_meters = m_nodes[i].dist_mm_q2 / 1000.f / (1 << 2);
-      lidarDataStream << distance_in_meters << ","
-                      << angle_in_degrees << ","
-                      << unsigned(m_nodes[i].quality) << ","
-                      << unsigned(m_nodes[i].flag);
-      if (i != m_nodeCount - 1) {
-        lidarDataStream << ",";
+    try {
+      lidarManager.grabScanData(m_nodes, m_nodeCount);
+      failedToGetScanDataCount = 0;
+    } catch (std::runtime_error& error) {
+      failedToGetScanDataCount++;
+      if (failedToGetScanDataCount == 10) {
+        std::cout << "WARNING: Failed to get lidar scan data 10 times in a row, restarting lidar manager." << std::endl;
+        lidarManager.quickStop();
+        lidarManager.quickStart();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      } else if (failedToGetScanDataCount <= 20) {
+        std::cout << "WARNING: Failed to get lidar scan data: " << error.what() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+      } else {
+        throw error;
       }
     }
+    lock2.unlock();
+    lock.lock();
+    if (failedToGetScanDataCount == 0 && !threadInterrupt) {
+      std::stringstream lidarDataStream;
+      for (int i = 0; i < m_nodeCount; i++) {
+        float angle_in_degrees = m_nodes[i].angle_z_q14 * 90.f / (1 << 14);
+        float distance_in_meters = m_nodes[i].dist_mm_q2 / 1000.f / (1 << 2);
+        lidarDataStream << distance_in_meters << ","
+                        << angle_in_degrees << ","
+                        << unsigned(m_nodes[i].quality) << ","
+                        << unsigned(m_nodes[i].flag);
+        if (i != m_nodeCount - 1) {
+          lidarDataStream << ",";
+        }
+      }
 
-    long long timeLog = std::chrono::duration_cast<std::chrono::nanoseconds>
-        (timeNow - TimeManager::getStartTime()).count();
-    std::string lidarData = lidarDataStream.str();
-    BOOST_LOG(m_logger) << logging::add_value("DataTimeStamp", timeLog)
-                        << m_nodeCount << "," << lidarData;
+      long long timeLog = std::chrono::duration_cast<std::chrono::nanoseconds>
+          (timeNow - TimeManager::getStartTime()).count();
+      std::string lidarData = lidarDataStream.str();
+      BOOST_LOG(m_logger) << logging::add_value("DataTimeStamp", timeLog)
+                          << m_nodeCount << "," << lidarData;
+    }
   }
 }
 
